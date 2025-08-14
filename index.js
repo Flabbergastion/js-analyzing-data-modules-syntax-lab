b8a883875280074efcb0c12424a59a3e115e0f07
Flabbergastion <maxgralpois@gmail.com>
2025-08-14 16:45:16 -0700

    Testing passed and code refactored

--- index.js
+++ index.js
@@ -4,3 +4,25 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+
+
+require('datejs');
+
+
+function combineUsers(...args) {
+  const combinedObject = {
+  users: [],
+};
+const mergeDate = Date.today().toString('M/d/yyyy');
+  combinedObject.merge_date = mergeDate;
+ for (const arg of args) { // Loop through each argument
+    if (Array.isArray(arg)) { // Check if the argument is an array
+      // Step 4: Merge Arrays using the spread operator
+      combinedObject.users = [...combinedObject.users, ...arg];
+    }
+  }
+  return combinedObject;
+
+
+}
